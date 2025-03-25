204685acbcbf031516ba46672ea8c669f4ff6ab6
mathiasvindinghansen <mvh@the-lead.dk>
2025-03-25 10:08:27 +0100

    [update]

--- patterns/test-pattern.php
+++ patterns/test-pattern.php
@@ -1,10 +1,32 @@
 <?php
 /**
- * Title: test pattern
+ * Title: Test Pattern Title
  * Slug: mvh-test-pattern
  */
 ?>
 
-<!-- wp:paragraph -->
-<p>test content for pattern</p>
-<!-- /wp:paragraph -->
+<!-- wp:columns -->
+<div class="wp-block-columns">
+    <!-- wp:column -->
+    <div class="wp-block-column">
+        <!-- wp:heading -->
+        <h1>title</h1>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus sunt eius cum possimus officia, distinctio atque porro velit!</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column -->
+    <div class="wp-block-column">
+        <!-- wp:core/image -->
+        <figure class="wp-block-image">
+            <img src="" alt="">
+        </figure>
+        <!-- /wp:core/image -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
