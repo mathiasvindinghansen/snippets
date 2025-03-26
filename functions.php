39cf99967cf78bd7ab9a551455c7c4293c944d77
mathiasvindinghansen <mvh@the-lead.dk>
2025-03-26 10:11:26 +0100

    [update]

--- functions.php
+++ functions.php
@@ -9,14 +9,14 @@ function mvh_enqueue_styles() {
     wp_enqueue_style('mvh-state', get_template_directory_uri() . '/css/state.css?dev=' . time());
     wp_enqueue_style('mvh-theme', get_template_directory_uri() . '/css/theme.css?dev=' . time());
 
-    wp_enqueue_style('highlight-css', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css');
+    wp_enqueue_style('mvh-highlightjs-css', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css');
 }
 
 function mvh_enqueue_scripts() {
     wp_enqueue_script("jquery");
 
     wp_enqueue_script('mvh-particles', get_template_directory_uri() . '/libraries/particles.min.js?dev=' . time());
-	wp_enqueue_script('mvh-highlightjs', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js', '', 'latest', true);
+	wp_enqueue_script('mvh-highlightjs-js', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js', '', 'latest', true);
 
     wp_enqueue_script('mvh-script', get_template_directory_uri() . '/js/script.js?dev=' . time(), ['mvh-particles, highlight-js']);
 }
