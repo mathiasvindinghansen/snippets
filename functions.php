d728d4770a1d2e63adb307e2846c542516f087c1
mathiasvindinghansen <mvh@the-lead.dk>
2025-03-26 10:30:28 +0100

    [update]

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function mvh_enqueue_scripts() {
 	wp_enqueue_script('mvh-highlight.js', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js');
     wp_enqueue_script('mvh-highlight.js-php', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/php.min.js');
 
-    wp_add_inline_script('mvh-inline', 'hljs.highlightAll();');
+    wp_enqueue_script('mvh-script', get_template_directory_uri() . '/js/script.js?dev=' . time(), ['mvh-particles']);
 }
 
 add_action('wp_enqueue_scripts', 'mvh_enqueue_styles');
